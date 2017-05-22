bb0d8fc6f3b3fdac3b71fb9b8940acfc760efa15
lingchong2015 <lingdirk2009@163.com>
2017-05-22 20:58:43 +0800

    new

---
+++ lcandroidlib/src/main/java/com/curry/stephen/lcandroidlib/ui/PieChart.java
@@ -0,0 +1,137 @@
+package com.curry.stephen.lcandroidlib.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+
+public class PieChart extends ViewGroup {
+
+    private float mTextX = 0.0f;
+
+    private float mPointerRadius = 2.0f;
+    private float mPointerX;
+    private float mPointerY;
+
+    private Paint mTextPaint;
+
+    public PieChart(Context context) {
+        super(context);
+    }
+
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PieChart(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    private void setLayerToSW(View view) {
+        if (!view.isInEditMode() && Build.VERSION.SDK_INT >= 11) {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+    }
+
+    private class PieView extends View {
+
+        private float mRotation = 0;
+        private Matrix mTransform = new Matrix();
+        private PointF mPivot = new PointF();
+
+        /**
+         * 构造一个饼块对象。
+         *
+         * @param context 上下文对象。
+         */
+        public PieView(Context context) {
+            super(context);
+        }
+
+        public void accelerate() {
+
+        }
+    }
+
+    /**
+     * 在饼图的上方绘制一个指针视图。
+     */
+    private class PointerView extends View {
+
+        /**
+         * 构造一个指针视图对象。
+         *
+         * @param context 上下文对象。
+         */
+        public PointerView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);// View的onDraw实现方法是空方法。
+
+            canvas.drawLine(mTextX, mPointerY, mPointerX, mPointerY, mTextPaint);
+            canvas.drawCircle(mPointerX, mPointerY, mPointerRadius, mTextPaint);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
